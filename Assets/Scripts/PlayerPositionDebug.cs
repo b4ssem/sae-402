4349d256a018c697568cd56591f7f7625a50e59e
b4ssem <bmeghiche06@gmail.com>
2026-03-19 14:37:16 +0100

    Ajout animation ventilateur

---
+++ Assets/Scripts/PlayerPositionDebug.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+ 
+ 
+public class PlayerPositionDebug : MonoBehaviour
+{
+    #if UNITY_EDITOR
+    [SerializeField]
+    private Transform debugTransform;
+ 
+ 
+    void Awake()
+    {
+        if (debugTransform)
+        {
+            transform.position = debugTransform.position;
+        }
+    }
+    #endif
+}
+ 
+ 
